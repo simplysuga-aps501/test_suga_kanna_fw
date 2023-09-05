a9f8b2537d53b27d4962ca6430a7f911fe92eacc
simplysuga-aps501 <simplysuga_aps501@avvaiyarpadasalai.org>
2023-09-05 12:16:49 -0400

    prod & dev

--- lib/main.dart
+++ lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Home Page for GIT'),
+      home: const MyHomePage(title: 'Flutter Home Page for GIT for prod & dev'),
     );
   }
 }
